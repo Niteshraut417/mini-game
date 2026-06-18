7aa47c77fbfffbddaf3801598c1c43e9a064c687
Niteshraut417 <niteshraut0417@gmail.com>
2026-06-18 23:38:35 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const words = ["coding", "github", "javascript", "game"];
+let currentWord = words[0];
+
+const wordDisplay = document.getElementById("word");
+const inputField = document.getElementById("input");
+
+wordDisplay.innerText = currentWord;
+
+inputField.addEventListener("input", () => {
+    if (inputField.value === currentWord) {
+        currentWord = words[Math.floor(Math.random() * words.length)];
+        wordDisplay.innerText = currentWord;
+        inputField.value = "";
+    }
+});
